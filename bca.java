03fdb45312825f8518db65e14b08d6140f2e74ca
mudiittk <110332330+mudiittk@users.noreply.github.com>
2022-09-04 11:29:02 +0530

    Add files via upload

---
+++ bca.java
@@ -0,0 +1,12 @@
+public class bca{
+String name="mudit";
+public static void main(String args[]){
+//String subject=new String[4];
+//String name="mudit";
+//string subject;
+//int roll;
+bca mudit=new bca();
+System.out.println(mudit.name);
+}
+}
+
