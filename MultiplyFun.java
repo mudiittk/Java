b51d4ac29a669d7b02cf2acb6da3d39f2e96dffa
mudiittk <110332330+mudiittk@users.noreply.github.com>
2022-10-02 11:39:30 +0530

    Add files via upload

---
+++ MultiplyFun.java
@@ -0,0 +1,14 @@
+public class MultiplyFun {
+    static int Multiply(int a, int b) {
+        return a * b;
+    }
+
+    static double Multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(MultiplyFun.Multiply(2, 4));
+        System.out.println(MultiplyFun.Multiply(2.2, 4.4));
+    }
+}
